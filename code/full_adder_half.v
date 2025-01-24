677e2ff252c4b0fe0bbd950f8b2a3b6ab5a6bbf1
Shivansh <shivanshmp1@gmail.com>
2025-01-24 17:46:38 +0530

    Update full_adder_half.v
    Fixed with the wires (ports are by default taken as  nets, but for internal signals we have to declare the type as nets(wire) or reg, etc)
    
    Used proper port names for port name connection.

--- code/full_adder_half.v
+++ code/full_adder_half.v
@@ -10,14 +10,16 @@ module fulladd(a, b, Cin, Sum, Carry);
 
 input a, b, Cin;
 output Sum, Carry;
+
+wire n_carry1, n_carry2, n_sum;
  
 /*
 instead of using assignment we will simply form port connections for 2 instances of half adders with appropriate logic
 we need to include the files that contain the halfadd module in the compile statement in CMD so iverilog knows 
 where to find the module
 */
- halfadd H1(.a(a), .b(b), .sum(n_sum), .carry(n_carry)); 
- halfadd H2(.a(n_sum), .b(Cin), .sum(sum), .carry(n_carry2));
+ halfadd H1(.a(a), .b(b), .S(n_sum), .C(n_carry1)); //This is called port connecting by name
+ halfadd H2(.a(n_sum), .b(Cin), .S(sum), .C(n_carry2));  
  assign Carry = n_carry2 ^ n_carry1; 
  
  
@@ -27,3 +29,22 @@ where to find the module
  */
 
 endmodule 
+
+/*
+Compile errors:
+
+D:\Software\GitHub Desktop\Verilog-Projects\code\full_adder_half.v:19: error: port ``sum'' is not a port of H1.
+D:\Software\GitHub Desktop\Verilog-Projects\code\full_adder_half.v:19: error: port ``carry'' is not a port of H1.
+D:\Software\GitHub Desktop\Verilog-Projects\code\full_adder_half.v:20: error: port ``sum'' is not a port of H2.
+D:\Software\GitHub Desktop\Verilog-Projects\code\full_adder_half.v:20: error: port ``carry'' is not a port of H2.
+D:\Software\GitHub Desktop\Verilog-Projects\code\full_adder_half.v:21: error: Unable to bind wire/reg/memory `n_carry1' in `fulladd'
+D:\Software\GitHub Desktop\Verilog-Projects\code\full_adder_half.v:21: error: Unable to elaborate r-value: (n_carry2)^(n_carry1)
+*/
+
+/*
+Fixes:
+
+there were no wires for internal signals: added wire n_carry1, n_carry2, n_sum;
+the names of the ports for the instantiation were wrong
+
+*/
