1be3a136f97785ce63d999bf0b27dd7de2085ace
Shivansh <shivanshmp1@gmail.com>
2025-01-27 19:01:21 +0530

    Update MUX.v
    Very stupid fixes

--- code/MUX/MUX.v
+++ code/MUX/MUX.v
@@ -4,11 +4,20 @@ in a MUX, 2^n inputs correspond to 1 output.
 there's n Switch Lines, 2^n Input Lines and 1 output line */
 
 //Output C = S&A + S'&B
-module mux_2x1(A,B, C, S)
+module mux_2x1(A,B, C, S);
 input A, B, S;
 output C;
 
-assign C = S&A + ~S&B; // '~' is the NOT
+assign C = (S & A) |(~S & B); // '~' is the NOT
 
 endmodule
 
+/*
+D:\Software\GitHub Desktop\Verilog-Projects\code\MUX\MUX.v:8: syntax error
+**/
+
+/*
+Fixes:
+Missed semi-colon
+used + instead of | for OR
+*/
